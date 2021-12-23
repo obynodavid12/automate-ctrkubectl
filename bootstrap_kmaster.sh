34413f6bce636145b90ef3bb01967c5a0f635830
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2021-12-23 15:37:31 -0500

    Update bootstrap_kmaster.sh

--- bootstrap_kmaster.sh
+++ bootstrap_kmaster.sh
@@ -6,13 +6,15 @@ kubeadm config images pull >/dev/null 2>&1
 echo "[TASK 2] Initialize Kubernetes Cluster"
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.33.100 >> /root/kubeinit.log 2>/dev/null
 
-echo "[TASK 3] Start using your cluster"
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+# echo "[TASK 3] Start using your cluster"
+# mkdir -p $HOME/.kube
+# sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 echo "[TASK 4] Deploy Weave network"
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" >/dev/null 2>&1
+# kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml >/dev/null 2>&1
+
 
 echo "[TASK 5] Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
