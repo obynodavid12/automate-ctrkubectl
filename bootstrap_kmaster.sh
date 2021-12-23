2d4fbd15d45f6019c14da350d76b47a47c33c54e
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2021-12-22 22:16:41 -0500

    Update bootstrap_kmaster.sh

--- bootstrap_kmaster.sh
+++ bootstrap_kmaster.sh
@@ -11,8 +11,8 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo "[TASK 3] Deploy Weave network"
+echo "[TASK 4] Deploy Weave network"
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" >/dev/null 2>&1
 
-echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
+echo "[TASK 5] Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
