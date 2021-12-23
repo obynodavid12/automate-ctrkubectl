5f423477fe2976b9afc646e3e20323b8397d5f31
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2021-12-22 22:15:51 -0500

    Update bootstrap_kmaster.sh

--- bootstrap_kmaster.sh
+++ bootstrap_kmaster.sh
@@ -13,7 +13,6 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 echo "[TASK 3] Deploy Weave network"
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" >/dev/null 2>&1
-# kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml >/dev/null 2>&1
 
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
